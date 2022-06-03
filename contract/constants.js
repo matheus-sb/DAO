a36c3efb0696e3cd9b6848135b2964e046be5259
matheus <matheus.sob@gmail.com>
2022-06-03 11:52:05 +0200

    Add contract and my-app to DAO project

---
+++ contract/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x14cfa695efE150548512000376d743333B059303";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
